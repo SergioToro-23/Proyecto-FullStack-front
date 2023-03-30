5ddc3d15799bb122ef8c8f0727df0013f92fd8c5
Sergio Toro <you@example.com>
2023-03-30 13:46:30 -0500

    agreg valid errores 2

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -3,52 +3,54 @@ import { CLIENTES } from './clientes.json';//importa desde un json la data
 import { Cliente } from './cliente';
 import { Observable } from 'rxjs';//ESTO ES PARA USAR STREAM, REACTIVE EXTENTION
 import { of } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';//cathc error, operador que intercepta el observable o flujo en busqueda de fallos, y si falla se obitenen este objeto dentro del operador .pipe()
 import { HttpClient, HttpHeaders } from '@angular/common/http';//para trabajar http
+
+//aca es donde estamos trabajando con nuestro datos, es donde estamos obteniendo la logica de negocio desde el backend
 
 @Injectable({//injectable, es para clases de servicios, representan logica de negocio
   providedIn: 'root'//se puede inyectar a otros componentes, via inyeccion de dependencias
 })
 export class ClienteService {//ESTA  clase es para majear los datos de los clientes
 
-  private urlEndPoint:string = 'http://localhost:8080/api/clientes'//ubicacion del back
-  private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'})//las cabeceras
+  private urlEndPoint: string = 'http://localhost:8080/api/clientes'//ubicacion del back
+  private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' })//las cabeceras
 
   constructor(private http: HttpClient) { }//definimos la variable http y se inyecta la referencia, para REST
 
-/* MODO NORMAL SINCRONO
-  getClientes(): Cliente[] {
-    return CLIENTES;
-  }*/
+  /* MODO NORMAL SINCRONO
+    getClientes(): Cliente[] {
+      return CLIENTES;
+    }*/
 
-//ESTE ES REACTIVO, RETORNA UN STREAM
-getClientes(): Observable<Cliente[]> {         //GET como en postman pero todos
-  
-  //este get devuelve un observable de tipo any generico, y con Cast se vuelve tipo Cliente
-  return this.http.get<Cliente[]>(this.urlEndPoint);//se usa this porque es un atributo de esta clase
-  //retorna el atributo http de esta clase, .el tipo de peticion y la url
+  //ESTE ES REACTIVO, RETORNA UN STREAM
+  getClientes(): Observable<Cliente[]> {         //GET como en postman pero todos
+
+    //este get devuelve un observable de tipo any generico, y con Cast se vuelve tipo Cliente
+    return this.http.get<Cliente[]>(this.urlEndPoint);//se usa this porque es un atributo de esta clase
+    //retorna el atributo http de esta clase, .el tipo de peticion y la url
 
 
-  //return of(CLIENTES);//Convertimos / creamos nuestro FLUJO OBSERVABLE a partir de los objetos clientes
-}//la idea es que maneje grandes flujos de datos de cualquier tipo, stream audio o video, o listas de objetos
-//patron de diseño observador: Tenemos un objeto observable, y tenemos observadores, que estan atentos obserbando un posible cambio en el sujeto
-//los observadores se suscriben al sujeto(el observable), y cuando cambia el estado, se notifica a los observadores y se dispara algun tipo de evento
+    //return of(CLIENTES);//Convertimos / creamos nuestro FLUJO OBSERVABLE a partir de los objetos clientes
+  }//la idea es que maneje grandes flujos de datos de cualquier tipo, stream audio o video, o listas de objetos
+  //patron de diseño observador: Tenemos un objeto observable, y tenemos observadores, que estan atentos obserbando un posible cambio en el sujeto
+  //los observadores se suscriben al sujeto(el observable), y cuando cambia el estado, se notifica a los observadores y se dispara algun tipo de evento
 
-//retorna un observable cliente, el objeto cliente que se creo en el apiRest
-create(cliente: Cliente): Observable<Cliente>{  //CREAR
+  //retorna un observable cliente, el objeto cliente que se creo en el apiRest
+  create(cliente: Cliente): Observable<Cliente> {  //CREAR
+    return this.http.post<Cliente>(this.urlEndPoint, cliente, { headers: this.httpHeaders })
+  }
 
-  return this.http.post<Cliente>(this.urlEndPoint, cliente, {headers: this.httpHeaders})
-}
+  getCliente(id: any): Observable<Cliente> {  //GET solo un cliente
+    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`)//a los argumentos le añade el id ya que asi esta el servicio
+  }
 
-getCliente(id: any): Observable<Cliente>{  //GET solo un cliente
-  return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`)//a los argumentos le añade el id ya que asi esta el servicio
-}
+  update(cliente: Cliente): Observable<Cliente> {  //PUT solo un cliente
+    return this.http.put<Cliente>(`${this.urlEndPoint}/${cliente.id}`, cliente, { headers: this.httpHeaders })//envia tres parametros, id, el objeto cliente, y cabecera
+  }
 
-update(cliente: Cliente): Observable<Cliente>{  //PUT solo un cliente
-  return this.http.put<Cliente>(`${this.urlEndPoint}/${cliente.id}`, cliente, {headers: this.httpHeaders})//envia tres parametros, id, el objeto cliente, y cabecera
-}
-
-delete(id: number): Observable<Cliente>{  //DELETE solo un cliente
-  return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`,{headers: this.httpHeaders})
-}
+  delete(id: number): Observable<Cliente> {  //DELETE solo un cliente
+    return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`, { headers: this.httpHeaders })
+  }
 
 }
--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -9,9 +9,9 @@ import Swal from 'sweetalert2';
 })
 export class ClientesComponent implements OnInit {
 
-  clientes!: Cliente[];    
+  clientes!: Cliente[];
 
-  constructor(private clienteService: ClienteService){}//declarar el atributo de la clase service en el constr, con inyeccion de dependencias se inyecta
+  constructor(private clienteService: ClienteService) { }//declarar el atributo de la clase service en el constr, con inyeccion de dependencias se inyecta
 
   ngOnInit(): void {
     //forma sincrona sin ser reactivo
@@ -19,11 +19,11 @@ export class ClientesComponent implements OnInit {
     //forma reactiva con stream
     this.clienteService.getClientes().subscribe(//suscribir o registrar el observador a los clientes
       CLIENTES => this.clientes = CLIENTES//function anonima, asigna en el atributo clientes, el valor que se recibe desde clientes service
-      );//osea el listado de clientes, cada vez que hay cambios
+    );//osea el listado de clientes, cada vez que hay cambios
 
   }
 
-  delete(cliente: Cliente): void{
+  delete(cliente: Cliente): void {
 
     const swalWithBootstrapButtons = Swal.mixin({
       customClass: {
@@ -32,7 +32,7 @@ export class ClientesComponent implements OnInit {
       },
       buttonsStyling: false
     })
-    
+
     swalWithBootstrapButtons.fire({
       title: 'Are you sure?',
       text: `¿Seguro que desea eliminar al cliente ${cliente.nombre} ${cliente.apellido}?`,
@@ -43,17 +43,17 @@ export class ClientesComponent implements OnInit {
       reverseButtons: true
     }).then((result) => {
       if (result.isConfirmed) {
-                 this.clienteService.delete(cliente.id).subscribe(
-                  response => {//cuando responda hace esto
-                    this.clientes = this.clientes.filter(cli => cli!== cliente)//si cada uno de los clientes de la lista es distinto al que vamos a eliminar, lo muestra en la lista                  
-                    swalWithBootstrapButtons.fire(
-                      'Deleted!',
-                      `Cliente ${cliente.nombre} eliminado con exito.`,
-                      'success'
-                    )
-                  }
-                 )        
-      } 
+        this.clienteService.delete(cliente.id).subscribe(
+          response => {//cuando responda hace esto
+            this.clientes = this.clientes.filter(cli => cli !== cliente)//si cada uno de los clientes de la lista es distinto al que vamos a eliminar, lo muestra en la lista                  
+            swalWithBootstrapButtons.fire(
+              'Deleted!',
+              `Cliente ${cliente.nombre} eliminado con exito.`,
+              'success'
+            )
+          }
+        )
+      }
     })
 
 
