564542c63da36914c3396da961b4cd46b76dd587
Sergio Toro <you@example.com>
2023-04-02 13:20:20 -0500

    validacion funcionamiento 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,17 @@ import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
 import { ClienteService } from './clientes/cliente.service';
 import { RouterModule, Routes } from '@angular/router';//IMPLEMENTACION DE RUTAS EN ANGULAR Y NAVEGACION
-import {HttpClientModule} from '@angular/common/http';//para comunicarse REST
+import { HttpClientModule } from '@angular/common/http';//para comunicarse REST
 import { FormComponent } from './clientes/form.component';//importa el componente
 import { FormsModule } from '@angular/forms';//importa formularios
 
 
-const routes: Routes =[//una constante que tiene un arreglo con las rutas tipo Routes
-  {path: '', redirectTo: '/clientes', pathMatch: 'full'},//el home '' va a clientes, y hace un match completo, osea va a la 3era opcion
-  {path: 'directivas', component: DirectivaComponent},
-  {path: 'clientes', component: ClientesComponent},//url clientes se asigna a componente clientecomponent
-  {path: 'clientes/form', component: FormComponent},
-  {path: 'clientes/form/:id', component: FormComponent}
+const routes: Routes = [//una constante que tiene un arreglo con las rutas tipo Routes
+  { path: '', redirectTo: '/clientes', pathMatch: 'full' },//el home '' va a clientes, y hace un match completo, osea va a la 3era opcion
+  { path: 'directivas', component: DirectivaComponent },
+  { path: 'clientes', component: ClientesComponent },//url clientes se asigna a componente clientecomponent
+  { path: 'clientes/form', component: FormComponent },
+  { path: 'clientes/form/:id', component: FormComponent }
 ];
 
 @NgModule({
@@ -26,9 +26,9 @@ const routes: Routes =[//una constante que tiene un arreglo con las rutas tipo R
     AppComponent,
     HeaderComponent,//aca se registra
     FooterComponent,
-     DirectivaComponent,
-      ClientesComponent,
-      FormComponent
+    DirectivaComponent,
+    ClientesComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
